6c7b33da08e3c29a3fe6e5d057d8fa988e65a539
Wessel Smit <wrsmit00@gmail.com>
2020-02-13 11:41:40 +0100

    cursor spotlight try

---
+++ script.js
@@ -0,0 +1,11 @@
+// window.addEventListener('mousemove', function (e) {
+//     const cursorHighlight = document.querySelector('main>div:nth-last-child(2)')
+
+//     if (window.matchMedia("(min-width: 1400px)").matches) {
+//         cursorHighlight.style.display = "block"
+//         cursorHighlight.style.left = e.pageX - (cursorHighlight.getBoundingClientRect().width / 2) + 17 + "px"
+//         cursorHighlight.style.top = e.pageY - (cursorHighlight.getBoundingClientRect().width / 2) + 17 + "px"
+//     } else {
+//         cursorHighlight.style.display = "none"
+//     }
+// })
